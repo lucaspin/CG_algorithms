1cd925d81b9a90c8a2c5dddc6474b04cf63c2120
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2016-04-17 13:37:34 -0300

    Initial Commit

---
+++ CG_algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CG_algorithms
+//
+//  Created by Lucas Pinheiro on 17/04/16.
+//  Copyright © 2016 Lucas Pinheiro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
