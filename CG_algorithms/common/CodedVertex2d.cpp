71943503f5b46229dc1fdd013617e6af4e3f124e
daltonbr <sacinopatinete@gmail.com>
2016-05-11 00:14:44 -0300

    Add CodedVertex2d
    This class CodedVertex2d() extends Vertex2d(), and will be used to
    implement the clipping algorithms

---
+++ CG_algorithms/common/CodedVertex2d.cpp
@@ -0,0 +1,96 @@
+/**
+ * CodedVertex2d.cpp
+ * We extend this class from Vertex2d, adding a regionCode field,
+ * to implement the Cohen-Sutherland algorithm
+ * @author Lucas Pinheiro (@lucaspin)
+ * @author Dalton Lima (@daltonbr)
+ */
+
+#include "CodedVertex2d.hpp"
+#include "Vertex2d.hpp"
+#include <vector>
+
+using namespace std;
+
+/**
+ * Constructor of the class
+ * @param {float x}
+ * @param {float y}
+ * @param {float y}
+ */
+CodedVertex2d::CodedVertex2d(float x, float y, float z):Vertex2d(x, y, z) {
+    vector<bool> regionCode = {false, false, false, false};
+    this->setRegionCode(regionCode);
+}
+
+/**
+ * Setter for the RegionCode
+ * @param _regionCode {vector<bool>}
+ */
+void CodedVertex2d::setRegionCode(vector<bool> _regionCode) {
+    this->regionCode = _regionCode;
+}
+
+/**
+ * Setter for the regionCode (top)
+ * @param _state {bool}
+ */
+void CodedVertex2d::setTopRegionCode(bool _state) {
+    this->regionCode[0] = _state;
+}
+
+/**
+ * Setter for the regionCode (bottom)
+ * @param _state {bool}
+ */
+void CodedVertex2d::setBottomRegionCode(bool _state) {
+    this->regionCode[1] = _state;
+}
+
+/**
+ * Setter for the regionCode (right)
+ * @param _state {bool}
+ */
+void CodedVertex2d::setRightRegionCode(bool _state) {
+    this->regionCode[2] = _state;
+}
+
+/**
+ * Setter for the regionCode (left)
+ * @param _state {bool}
+ */
+void CodedVertex2d::setLeftRegionCode(bool _state) {
+    this->regionCode[3] = _state;
+}
+
+/**
+ * Get the regionCode (top)
+ * @return {bool}
+ */
+bool CodedVertex2d::getTopRegionCode() {
+    return this->regionCode[0];
+}
+
+/**
+ * Get the regionCode (bottom)
+ * @return {bool}
+ */
+bool CodedVertex2d::getBottomRegionCode() {
+    return this->regionCode[1];
+}
+
+/**
+ * Get the regionCode (right)
+ * @return {bool}
+ */
+bool CodedVertex2d::getRightRegionCode() {
+    return this->regionCode[2];
+}
+
+/**
+ * Get the regionCode (left)
+ * @return {bool}
+ */
+bool CodedVertex2d::getLeftRegionCode() {
+    return this->regionCode[3];
+}
---
+++ CG_algorithms/common/CodedVertex2d.hpp
@@ -0,0 +1,31 @@
+/**
+ * CodedVertex2d.hpp
+ * We extend this class from Vertex2d, adding a regionCode field,
+ * to implement the Cohen-Sutherland algorithm
+ * @author Lucas Pinheiro (@lucaspin)
+ * @author Dalton Lima (@daltonbr)
+ */
+
+#ifndef CodedVertex2d_hpp
+#define CodedVertex2d_hpp
+
+#include "CodedVertex2d.hpp"
+#include "Vertex2d.hpp"
+#include <vector>
+
+class CodedVertex2d : Vertex2d {
+private:
+    std::vector<bool> regionCode;
+public:
+    CodedVertex2d(float x, float y, float z);
+    void setRegionCode(std::vector<bool> _regionCode);
+    void setTopRegionCode(bool _state);
+    void setBottomRegionCode(bool _state);
+    void setRightRegionCode(bool _state);
+    void setLeftRegionCode(bool _state);
+    bool getTopRegionCode();
+    bool getBottomRegionCode();
+    bool getRightRegionCode();
+    bool getLeftRegionCode();
+};
+#endif /* CodedVertex2d_hpp */
